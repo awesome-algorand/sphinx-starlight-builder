0eafc6b81a8becef97959d41a0d96bedf693d145
Joe Polny <joepolny@gmail.com>
2024-12-10 08:30:50 -0500

    markdown_uri_doc_suffix md -> mdx

--- sphinx_markdown_builder/__init__.py
+++ sphinx_markdown_builder/__init__.py
@@ -11,7 +11,7 @@ __docformat__ = "reStructuredText"
 def setup(app):
     app.add_builder(MarkdownBuilder)
     app.add_config_value("markdown_http_base", "", False)
-    app.add_config_value("markdown_uri_doc_suffix", ".md", False)
+    app.add_config_value("markdown_uri_doc_suffix", ".mdx", False)
     app.add_config_value("markdown_anchor_sections", False, False)
     app.add_config_value("markdown_anchor_signatures", False, False)
     app.add_config_value("markdown_docinfo", False, False)
